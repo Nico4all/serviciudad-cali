5979a2dee276169d27701f3aaf409c6dd7c225de
Nicolas Sanchez <enicolassancheztobar@gmail.com>
2025-10-20 16:12:42 -0500

    first commit

---
+++ src/test/java/com/serviciudad/serviciudad_cali/ServiciudadCaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviciudad.serviciudad_cali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciudadCaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
